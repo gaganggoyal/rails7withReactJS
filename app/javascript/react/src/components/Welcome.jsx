acfbc97fc5c9f0a2aa1bab739a17bce378680d04
gagan goyal <goyalgagan82@gmail.com>
2023-03-09 21:42:33 +0530

    Add build:css scripts

---
+++ app/javascript/react/src/components/Welcome.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const Welcome = () => {
+  return (
+    <div className="container">
+      <h1>HELLO WORLD! Welcome to Rails 7 </h1>
+    </div>
+  );
+};
+
+document.addEventListener("DOMContentLoaded", () => {
+  ReactDOM.render(<Welcome />, document.getElementById('welcome'));
+});
+
+export default Welcome;
