5f4ccf6e7b88e0aa1114f538a6bf87337484b001
gagan goyal <goyalgagan82@gmail.com>
2023-03-10 14:37:54 +0530

    Add dislike button & html css style for button space

--- app/javascript/react/src/components/QuestionDetail.jsx
+++ app/javascript/react/src/components/QuestionDetail.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 
 const QuestionDetail = (props) => {
   const [likeCount, setLikeCount] = useState(0);
+  const [dislikeCount, setDislikeCount] = useState(0);
 
   return (
     <div className="card rounded-0 mt-3">
@@ -13,12 +14,27 @@ const QuestionDetail = (props) => {
         <button
           type="button"
           className="btn btn-primary position-relative"
-          onClick={() => setLikeCount(likeCount + 1)}
+          onClick={() => setLikeCount(likeCount + 1)} style={{marginRight:1 + 'em'}}
         >
           Like
           {likeCount > 0 ? (
             <span className="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">
               {likeCount}
+            </span>
+          ) : (
+            ""
+          )}
+        </button>
+
+        <button
+          type="button"
+          className="btn btn-primary position-relative"
+          onClick={() => setDislikeCount(likeCount + 1)}
+        >
+          Dislike
+          {dislikeCount > 0 ? (
+            <span className="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">
+              {dislikeCount}
             </span>
           ) : (
             ""
