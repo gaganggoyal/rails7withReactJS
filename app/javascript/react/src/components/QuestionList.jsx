bab5f92caf9b30b75b2cc7df402920f4cad2099d
gagan goyal <goyalgagan82@gmail.com>
2023-03-10 22:32:13 +0530

    Add dropdown menu with error alert msg

--- app/javascript/react/src/components/QuestionList.jsx
+++ app/javascript/react/src/components/QuestionList.jsx
@@ -3,22 +3,43 @@ import ReactDOM from "react-dom";
 import QuestionDetail from "./QuestionDetail";
 
 const QuestionList = () => {
-  const [questionList, setQuestionList] = useState([])
+  const questionTags = [
+    { label: "All", value: 0 },
+    { label: "Ruby", value: 1 },
+    { label: "Rails", value: 2 },
+    { label: "React", value: 3 },
+    { label: "Bootstrap", value: 4 },
+    { label: "Javascript", value: 5 }
+  ];
 
-  const questionsUrl = 'http://localhost:3000/api/v1/questions'
+  const [questionList, setQuestionList] = useState([]);
+  const [selectedOption, setSelectedOption] = useState(questionTags[0].value);
+
+  const questionsUrl = "http://localhost:3000/api/v1/questions";
 
   const fetchQuestionList = () => {
     fetch(questionsUrl)
-    .then((response) => response.json())
-    .then((data) => {
-      console.log(data)
-      setQuestionList(data)
-    })
-  }
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        setQuestionList(data);
+      });
+  };
 
   useEffect(() => {
-    fetchQuestionList()
-  }, [])
+    fetchQuestionList();
+  }, []);
+
+  const updateSelectedItem = (event) => {
+    setQuestionList([]);
+    setSelectedOption(event.target.value);
+    fetch(questionsUrl + `?tags=${questionTags[event.target.value].label}`)
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        setQuestionList(data);
+      });
+  };
 
   // const questionList = [
   //   {
@@ -76,6 +97,18 @@ const QuestionList = () => {
   return (
     <div className="row">
       <div className="col-lg-10 mx-auto">
+        <p className="lead fw-bold">Filter Questions by Tags</p>
+        <select
+          className="form-select form-select-lg"
+          value={selectedOption}
+          onChange={(event) => updateSelectedItem(event)}
+        >
+          {questionTags.map((tag) => (
+            <option key={tag.value} value={tag.value}>
+              {tag.label}
+            </option>
+          ))}
+        </select>
         {questionList.map((question) => (
           <QuestionDetail question={question} key={question.id} />
         ))}
