a0d7bdf01eb564e5496172072e919fd9a37d8bb6
gagan goyal <goyalgagan82@gmail.com>
2023-03-10 12:31:24 +0530

    Add question list data in db & question model

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,63 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+questionList = Question.create([
+  {
+    title: "How to check if a key is present in a Hash?",
+    tag: "Ruby",
+  },
+  {
+    title: "What is the difference between string and symbol?",
+    tag: "Ruby",
+  },
+  {
+    title: "What will happen if you add two same keys in Hash?",
+    tag: "Ruby",
+  },
+  {
+    title: "How to delete a given key from hash?",
+    tag: "Ruby",
+  },
+  {
+    title: "How to check if two hashes are identicle?",
+    tag: "Ruby",
+  },
+  {
+    title: "How to combine two hashes in Ruby?",
+    tag: "Ruby",
+  },
+  {
+    title: "How to get unique keys from two hashes in Ruby?",
+    tag: "Ruby",
+  },
+  {
+    title: "What does the hash key?, key? member? include? methods in hash?",
+    tag: "Ruby",
+  },
+  {
+    title: "What are blocks in ruby?",
+    tag: "Ruby",
+  },
+  {
+    title: "Does the order of keys matters to compare two hashes in Ruby?",
+    tag: "Ruby",
+  },
+  {
+    title: "What is the key property in React?",
+    tag: "React",
+  },
+  {
+    title: "What are hooks in React?",
+    tag: "React",
+  },
+  {
+    title: "What is useState in React?",
+    tag: "React",
+  },
+
+  {
+    title: "What is createRoot in React?",
+    tag: "React",
+  }
+])
