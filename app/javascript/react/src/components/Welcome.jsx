e91d010b9e720a285b440fdc9a63ccc818ed52e9
gagan goyal <goyalgagan82@gmail.com>
2023-03-10 11:40:28 +0530

    Add useState Hook

--- app/javascript/react/src/components/Welcome.jsx
+++ app/javascript/react/src/components/Welcome.jsx
@@ -1,16 +1,26 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
+import QuestionList from "./QuestionList";
 
 const Welcome = () => {
   return (
     <div className="container">
       <h1>HELLO WORLD! Welcome to Rails 7 </h1>
+      <p className="lead">This is the first lecture</p>
+      <QuestionList />
     </div>
   );
 };
 
-document.addEventListener("DOMContentLoaded", () => {
-  ReactDOM.render(<Welcome />, document.getElementById('welcome'));
-});
+const root = ReactDOM.createRoot(document.getElementById('welcome'));
+root.render(
+  <React.StrictMode>
+    <Welcome />
+  </React.StrictMode>
+);
+
+// document.addEventListener("DOMContentLoaded", () => {
+//   ReactDOM.render(<Welcome />, document.getElementById("welcome"));
+// });
 
 export default Welcome;
